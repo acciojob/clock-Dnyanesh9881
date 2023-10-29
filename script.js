9caffa79395b6320cd855c75346e22a6934d2ffa
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 02:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let timer=document.getElementById("timer");
+ var display = new Date().toLocaleTimeString();
+ timer.innerHTML = display;
+ setTimeout(displayClock, 1000); 
