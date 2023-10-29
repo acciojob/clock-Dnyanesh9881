3de9f29509221c7c562d6e2fd0a3a5640443c89b
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 02:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 let timer=document.getElementById("timer");
  var display = new Date().toLocaleTimeString();
- timer.innerHTML = display;
- setTimeout(displayClock, 1000); 
+let date=new Date();
+ timer.innerHTML =date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear()+", "+display;
+ 
